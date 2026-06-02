6393b0c62ff71107678a4dab276ef286e1b92b5e
Theknower22 <khaledkane7@gmail.com>
2026-06-02 04:32:56 +0300

    add express dependency

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server is working 🚀");
+  res.send("Backend is working 🚀");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
